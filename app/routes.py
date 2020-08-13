c5c49f3b0fa400a8d7500be9c5cc6b2e3c82cc64
Cheng <44055703+UncleChicken@users.noreply.github.com>
2020-08-13 08:35:47 +0800

    Minor changes
    added some content for landing-page.html, trying to get routes.py to display that content, no success yet

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template, flash, redirect, url_for, request, jsonify, send_from_directory
 from app import app
 
 @app.route('/')
 @app.route('/index')
-def index():
-    return "Hello, world!"
+def landing_page():
+    return "Hello"
+    # return render_template("landing-page.html", title="Welcome")
