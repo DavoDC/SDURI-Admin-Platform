92b4ee8a7015271711a17dfc40ff2d4b611159e5
Cheng <44055703+UncleChicken@users.noreply.github.com>
2020-08-18 07:27:42 +0800

    Debugging
    render_template now works

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,18 @@
 from flask import render_template, flash, redirect, url_for, request, jsonify, send_from_directory
 from app import app
+# from app.forms import Login, Registration, AdminAddAccount, ChangePassword, SubAddQuestion, AdminAddQuestions, AdminAddQuiz, MainFormOptions, SubFormQuestion, SubAddOption
+from flask_login import current_user, login_user, logout_user, login_required, login_manager
+from werkzeug.urls import url_parse
+from werkzeug.exceptions import HTTPException, Forbidden
+from functools import wraps
+# from app.controllers.UserController import UserController
+# from app.controllers.QuizController import QuizController, QuestionType
+# from app.controllers.QuizAttemptController import QuizAttemptController
+from datetime import datetime
+import json
 
 @app.route('/')
 @app.route('/index')
 def landing_page():
-    return "Hello"
-    # return render_template("landing-page.html", title="Welcome")
+    # return "Hello"
+    return render_template("landing-page.html", title="Welcome")
