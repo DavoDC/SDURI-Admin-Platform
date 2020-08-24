22949b42af0cfa0fe7b63cff8f1fb771db3e87cd
vummang <53401737+vummang@users.noreply.github.com>
2020-08-25 01:59:23 +0800

    Add create_db.py

---
+++ create_db.py
@@ -0,0 +1,44 @@
+import os, subprocess 
+from app import db
+from app.models import User
+
+# Variables intialization
+a = "flask db init"
+b = "flask db migrate"
+c = "flask db upgrade"
+
+# Removing any previous migrations folder and database file for flexibility
+if (os.system("rm -r migrations")):
+  pass
+if (os.system("rm *.db")):
+  pass
+
+# Initialization of database 
+commands = [a, b, c]
+for command in commands:
+  os.system(command)
+
+# The following myusers dictionary format is: "email":["name", "password", "role"]
+myusers = {"student1@students.com":["student1","st","" ], 
+          "super1@supers.com":["supervisor1", "s", "Supervisor"], 
+          "admin1@admins.com":["admin1", "a", "Administrator"]}
+
+# Inserting users in to database from myusers (above)
+for email, details in myusers.items():
+  u = User()
+  u.set_user_email(email)
+  u.set_user_name(details[0])
+  u.set_user_password(details[1])
+  u.set_user_role(details[2])
+  db.session.add(u)
+  db.session.commit()
+
+# Migrating and upgrading database after insertion 
+commands_2 = [b, c]
+for command in commands_2:
+  os.system(command)
+
+
+
+
+
