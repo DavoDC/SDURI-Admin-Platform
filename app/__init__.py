4491f22cf5af5419afaefe0b556c310c2024fb4c
mang <vumsuanmang@gmail.com>
2020-08-28 17:27:30 +0800

    Add auth_forms.py, auth_routes.py and forgot_password.html

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,10 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-# from flask_login import LoginManager
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_mail import Mail
 
 from flask_admin import Admin
 from flask_bootstrap import Bootstrap
@@ -15,8 +18,13 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-# login = LoginManager(app) # TODO: causes Missing user_loader or request_loader exception when uncommented
-# login.login_view = 'login'
+login = LoginManager(app) # TODO: causes Missing user_loader or request_loader exception when uncommented
+login.login_view = 'login'
+
+
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+mail = Mail(app)
 
 Bootstrap(app)
 admin = Admin(app, name='Admin', template_mode='bootstrap3')
