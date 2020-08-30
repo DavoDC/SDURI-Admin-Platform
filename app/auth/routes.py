646c76a166e0f5b2d8bc83418f44a1d4faa1c31e
David C <45155292+DavoDC@users.noreply.github.com>
2020-08-30 10:35:23 +0800

    Did formatting/beautifying for all Python/HTML code

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,11 +1,18 @@
-from flask import render_template, flash, redirect, url_for, request
-from flask_login import current_user, login_required, login_user, logout_user
-from werkzeug.urls import url_parse
-
 from app import db
 from app.auth import bp
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm
+from app.forms import RegistrationForm
 from app.models import User
+from flask import flash
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
+from flask_login import current_user
+from flask_login import login_required
+from flask_login import login_user
+from flask_login import logout_user
+from werkzeug.urls import url_parse
 
 
 # @bp.route('/')  # methods=['GET', 'POST'])
@@ -18,69 +25,69 @@ from app.models import User
 @bp.route('/admin/<username>')
 @login_required
 def admin(username):
-  user = User.query.filter_by(name=username).first_or_404()
-  users = User.query.all()
-  return render_template('auth/admin.html', user=user, title='Admin', users=users)
+    user = User.query.filter_by(name=username).first_or_404()
+    users = User.query.all()
+    return render_template('auth/admin.html', user=user, title='Admin', users=users)
 
 
 @bp.route('/supervisor/<username>')
 @login_required
 def supervisors(username):
-  user = User.query.filter_by(name=username).first_or_404()
-  return render_template('supervisors.html', user=user, title='Supervisor')
+    user = User.query.filter_by(name=username).first_or_404()
+    return render_template('supervisors.html', user=user, title='Supervisor')
 
 
 @bp.route('/student/<username>')
 @login_required
 def students(username):
-  user = User.query.filter_by(name=username).first_or_404()
-  return render_template('students.html', user=user, title='Student')
+    user = User.query.filter_by(name=username).first_or_404()
+    return render_template('students.html', user=user, title='Student')
 
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
-  if current_user.is_authenticated:
-    return redirect(url_for('auth.index'))
-  form = LoginForm()
-  if form.validate_on_submit():
-    user = User.query.filter_by(email=form.email.data).first()
-    if user is None or not user.check_password(form.password.data):
-      flash('Invalid email or password')
-      return redirect(url_for('auth.login'))
+    if current_user.is_authenticated:
+        return redirect(url_for('auth.index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid email or password')
+            return redirect(url_for('auth.login'))
 
-    login_user(user, remember=form.remember_me.data)
-    # next_page = request.args.get('next')
-    # if not next_page or url_parse(next_page).netloc != '':
-    #   next_page = url_for('index')
-    # return redirect(next_page)
-    # return redirect(url_for('index.html')) # Use if next_page or next variable is not used
-    if user.role == "Administrator":
-      return redirect(url_for('admin', username=current_user.name))
-    elif user.role == "Supervisor":
-      return redirect(url_for('supervisors', username=current_user.name))
-    else:
-      return redirect(url_for('students', username=current_user.name))
-  return render_template('auth/login.html', title='Sign In', form=form)
+        login_user(user, remember=form.remember_me.data)
+        # next_page = request.args.get('next')
+        # if not next_page or url_parse(next_page).netloc != '':
+        #   next_page = url_for('index')
+        # return redirect(next_page)
+        # return redirect(url_for('index.html')) # Use if next_page or next variable is not used
+        if user.role == "Administrator":
+            return redirect(url_for('admin', username=current_user.name))
+        elif user.role == "Supervisor":
+            return redirect(url_for('supervisors', username=current_user.name))
+        else:
+            return redirect(url_for('students', username=current_user.name))
+    return render_template('auth/login.html', title='Sign In', form=form)
 
 
 @bp.route('/logout')
 def logout():
-  logout_user()
-  return redirect(url_for('index'))
+    logout_user()
+    return redirect(url_for('index'))
 
 
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
-  if current_user.is_authenticated:
-    return redirect(url_for('index'))
-  form = RegistrationForm()
-  if form.validate_on_submit():
-    user = User(name=form.username.data,
-                # user_fullname=form.user_fullname.data,
-                email=form.email.data)
-    user.set_user_password(form.password.data)
-    db.session.add(user)
-    db.session.commit()
-    flash('Congratulations, you are now a registered user!')
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(name=form.username.data,
+                    # user_fullname=form.user_fullname.data,
+                    email=form.email.data)
+        user.set_user_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
     return redirect(url_for('auth.login'))
-  return render_template('register.html', title='Register', form=form)
+return render_template('register.html', title='Register', form=form)
--- app/forms.py
+++ app/forms.py
@@ -1,36 +1,42 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 from app.models import User
+from flask_wtf import FlaskForm
+from wtforms import BooleanField
+from wtforms import PasswordField
+from wtforms import StringField
+from wtforms import SubmitField
+from wtforms.validators import DataRequired
+from wtforms.validators import Email
+from wtforms.validators import EqualTo
+from wtforms.validators import ValidationError
 
 class LoginForm(FlaskForm):
-  email = StringField('Email', validators=[DataRequired(), Email()])
-  password = PasswordField('Password', validators=[DataRequired()])
-  remember_me = BooleanField('Remember Me')
-  submit = SubmitField('Sign In')
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
 
-  # def validate_email(self, email):
-  #   user = User.query.filter_by(email=email.data).first()
-  #   if user is not None:
-  #     pass
-  #   else:
-  #     raise ValidationError('Please register.')
+    # def validate_email(self, email):
+    #   user = User.query.filter_by(email=email.data).first()
+    #   if user is not None:
+    #     pass
+    #   else:
+    #     raise ValidationError('Please register.')
 
 class RegistrationForm(FlaskForm):
-  username = StringField('Preferred name', validators=[DataRequired()])
-  # user_fullname = StringField('Full name', validators=[DataRequired()])
-  email = StringField('Email', validators=[DataRequired(), Email()])
-  password = PasswordField('Password', validators=[DataRequired()])
-  password2 = PasswordField(
-        'Confirm Password', validators=[DataRequired(), EqualTo('password')])
-  submit = SubmitField('Register')
+    username = StringField('Preferred name', validators=[DataRequired()])
+    # user_fullname = StringField('Full name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+                              'Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
 
-  def validate_username(self, username):
-    user = User.query.filter_by(name=username.data).first()
-    if user is not None:
-      raise ValidationError('Please use a different username.')
+    def validate_username(self, username):
+        user = User.query.filter_by(name=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
   
-  def validate_email(self, email):
-    user = User.query.filter_by(email=email.data).first()
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
     if user is not None:
-      raise ValidationError('Please use a different email address.')
+        raise ValidationError('Please use a different email address.')
