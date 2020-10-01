390eee2ed39034198e25e3a9260e725893326429
vummang <53401737+vummang@users.noreply.github.com>
2020-10-02 03:04:19 +0800

    Update config.py; Add email settings

--- config.py
+++ config.py
@@ -12,3 +12,17 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, database_name)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
+        
+    # mail settings
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+
+    # gmail authentication
+    MAIL_USERNAME = "sduri.pro@gmail.com" # s.environ['APP_MAIL_USERNAME']
+    MAIL_PASSWORD = "cits32**" # os.environ['APP_MAIL_PASSWORD']
+
+    # mail accounts
+    MAIL_DEFAULT_SENDER = 'from@example.com'
+    
